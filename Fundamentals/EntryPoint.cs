c165afcf15f1656aa701c48e2ffb427182336592
sujunepal <suzunepal.sn@gmail.com>
2023-02-23 09:05:04 +0545

    my commit

--- Fundamentals/EntryPoint.cs
+++ Fundamentals/EntryPoint.cs
@@ -1,9 +1,77 @@
-﻿Assignments obj1=new();
-int res=obj1.CalculateProductOfSquare(2,2); // return type bhayeko bhayera res
-Console.WriteLine("The product of squares of two supplied numbers is: "+res);
+﻿using System;
+using Basics;
+using Inheritance;
+using IO;
+//MethodAssignments ma = new();
 
-float m= obj1.CalculateBMI(58,5.5f);
-Console.WriteLine(m);
+// string m = ma.CalulateBMI(77.5f, 5.7f);
+// string m1 = ma.CalulateBMI(89.5f, 5.10f);
+// string m2 = ma.CalulateBMI(50.5f, 5.3f);
+// string m3 = ma.CalulateBMI(60f, 5.1f);
 
-double cube=obj1.CalculateCubeRootOfNumber(3);
-Console.WriteLine("The cuberoot of a number is: "+cube);
+// Console.WriteLine($"{m}, {m1}");
+
+// Console.WriteLine(m2);
+// Console.WriteLine(m3);
+
+// var car1 = new OilEngineVehicle("Car", "Nexon", "Tata", 4, 15.5f, "BA.33.PA 234", 1500f);
+// car1.PrintDetails();
+
+// Phone phone1 = new();
+// Phone.material = "sadfasf asf";
+// Phone.PrintDetails();
+
+// phone1.Brand = "A";
+// phone1.Model = "IPhone 14";
+// phone1.ReleaseDate = new DateTime(2023, 12, 12);
+// var f = phone1.ChipManufacturer;
+
+
+// Top-level statements
+// public static void Main() is implicitly there
+
+//MethodLearner ml = new();
+// ml.Display();
+// ml.Display("Hi");
+// ml.Display("Welcome");
+// ml.Display();
+
+// ml.Add(34.3f, 56.5f);
+// ml.Add(3445.3345f, 123.345f);
+// ml.Add(123124.3f, 756.798f);
+
+// var s = ml.Greet();
+// Console.WriteLine(s);
+
+// var d = ml.GetDay();
+// Console.WriteLine(d);
+
+// float f = 12.3f, se = 45.6f, t = 67.12f;
+// var sum = ml.Add(f, se, t);
+// var tt = $"{f} + {se} + {t} = {sum}";
+// Console.WriteLine(tt);
+
+// var p = ml.Multiply(34.5, 67.7, 45.12);
+// var p1 = ml.Multiply(34.5, 67.7);
+
+// var p2 = ml.SumOfSquares(34.5, 3, 4, 5, 6, 7);
+// var p3 = ml.SumOfSquares(34.5, 3, 4, 5, 6, 7, 4, 4, 6, 7, 8, 8, 3, 3, 6, 67, 78);
+
+// // Named arguments
+// var p5 = ml.Multiply(z: 34.5, x: 67.7, y: 45.12);
+
+// var x = ml.CalculateMin(3, 4, 5, 6, 7, 8, 12, 98, 2, 45, -1);
+// var (min, max) = ml.CalculateMinMax(3, 4, 5, 6, 7, 8, 12, 98, 2, 45, -1);
+
+
+// Generics<string> gn = new Generics<string>();
+
+// gn.Print<string>("Nepal is beautiful country");
+// gn.Print<byte>(79);
+// gn.Print<bool>(false);
+
+// gn.Greet("Hello there!");
+
+
+FDHandling fd = new();
+fd.LearnMore();
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
     1.Method to calculate product of squares of two supplied numbers.
     2.Method to calculate cuberoot of a number.
     3.Method to calculate BMI index of a person.
+
